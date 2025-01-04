6375a161e2006044ec7f8c645469058cf1b8be20
Gian Marco Simonazzi <gianmarco.simonazzi@studenti.unipr.it>
2025-01-04 11:30:32 +0100

    Isolated agent implementation. Rewrote output

---
+++ query.py
@@ -0,0 +1,41 @@
+import simple_system
+from langchain_core.messages.tool import ToolMessage
+
+def main():
+    config = {"configurable": {"thread_id": "1"}}
+
+    graph = simple_system.get_graph()
+    simple_system.show_graph(graph)
+
+    user_input = input("Query: ")
+    #user_input = 'llm-based agents'
+
+    # The config is the **second positional argument** to stream() or invoke()!
+    events = graph.stream(
+        {"messages": [{"role":"user","content": user_input}]}, config, stream_mode="values"
+    )
+    for event in events:
+        #print(event)
+        input('Press Enter to continue')
+        last_message = event["messages"][-1]
+        if not isinstance(last_message,ToolMessage):
+            last_message.pretty_print() #stream_mode='value'
+        else:
+            print('ToolMessage\n')
+        #stream_mode='debug'
+        '''if event['type'] == 'checkpoint':
+            if event['payload']['values']["messages"]:
+                event['payload']['values']["messages"][-1].pretty_print() 
+        elif event['type'] == 'task':
+            event['payload']['input']["messages"][-1].pretty_print() 
+        elif event['type'] == 'value':
+            event["messages"][-1].pretty_print()
+        elif event['type'] == 'task_result':
+            print('task')
+            event["payload"]['result'][0][-1][0].pretty_print()
+        else:
+            event["messages"][-1].pretty_print()'''
+        #print(event['type'])
+
+if __name__ == "__main__":
+    main()
--- simple_system.py
+++ simple_system.py
@@ -8,8 +8,8 @@ from langgraph.prebuilt import ToolNode
 from langgraph.graph import StateGraph, MessagesState
 from langgraph.graph.graph import START, END
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import MemorySaver
 from langchain.tools.retriever import create_retriever_tool
+from langgraph.checkpoint.memory import MemorySaver
 
 from typing import Annotated
 from typing_extensions import TypedDict
@@ -17,6 +17,7 @@ from typing_extensions import TypedDict
 import qdrant_tool
 
 import io, os
+import time
 from PIL import Image
 
 class State(TypedDict):
@@ -37,15 +38,15 @@ llama = ChatOllama(model = "llama3.2:3b", temperature = 0.7, num_predict = 256, 
 llama_long = ChatOllama(model = "llama3.2:3b", temperature = 0.8, num_predict = 1024, base_url="http://192.168.1.24:11434")
 qwq = ChatOllama(model = "qwq", temperature = 0.8, num_predict = 1024, base_url="http://192.168.1.24:11434")
 
-'''DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
+DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
 
 llm = ChatOpenAI(
     model="deepseek-chat", 
     api_key=DEEPSEEK_API_KEY, 
     base_url="https://api.deepseek.com",
     temperature=0.8,
-    max_tokens=256
-)'''
+    max_tokens=512
+)
 
 qdrant_retriever = create_retriever_tool(
     qdrant_tool.Qdrant_tool(host='192.168.1.26',port=6555,collection='Gruppo1',top_k=2),
@@ -74,86 +75,61 @@ writer_prompt = PromptTemplate(
     input_variables=["context", "question"],
 )
 
+
 def writer(state: State):
     print('In writer')
-    print(state)
+    #print(state)
     messages = state["messages"]
 
     question = messages[0].content
     docs = messages[-1].content
 
-    model = writer_prompt | llama_long
+    model = writer_prompt | llama
     response = model.invoke({'context':docs, 'question': question})
 
-    print(response)
+    #print(response)
+
+    #with open(f'temp/output-{str(int(time.time()))}.txt','w') as f:
+    #    f.write(response.content)
+
     return {"messages": [response]}
 
 def evaluator(state: State):
     print('In evaluator')
     #print(state)
-    exit()
     return {'messages':state['messages']}
 
 def should_continue(state: State):
     return END # placeholder
 
-memory = MemorySaver()
 
-graph_builder = StateGraph(MessagesState)
-graph_builder.add_node("researcher",researcher)
-graph_builder.add_node("writer",writer)
-graph_builder.add_node("evaluator",evaluator)
-retrieve = ToolNode([qdrant_retriever])
-graph_builder.add_node("retriever",retrieve)
+def get_graph():
+    graph_builder = StateGraph(MessagesState)
+    graph_builder.add_node("researcher",researcher)
+    graph_builder.add_node("writer",writer)
+    graph_builder.add_node("evaluator",evaluator)
+    retrieve = ToolNode([qdrant_retriever])
+    graph_builder.add_node("retriever",retrieve)
 
-'''graph_builder.add_conditional_edges(
-    "researcher",
-    # Assess agent decision
-    tools_condition,
-    {
-        # Translate the condition outputs to nodes in our graph
-        "tools": "retriever",
-        END: "writer",
-    },
-)'''
+    '''graph_builder.add_conditional_edges(
+        "researcher",
+        # Assess agent decision
+        tools_condition,
+        {
+            # Translate the condition outputs to nodes in our graph
+            "tools": "retriever",
+            END: "writer",
+        },
+    )'''
 
-graph_builder.add_edge(START, "researcher")
-graph_builder.add_edge("researcher", "retriever")
-graph_builder.add_edge("retriever", "writer")
-graph_builder.add_edge("writer", "evaluator")
-graph_builder.add_conditional_edges("evaluator", should_continue, ["writer",END])
+    graph_builder.add_edge(START, "researcher")
+    graph_builder.add_edge("researcher", "retriever")
+    graph_builder.add_edge("retriever", "writer")
+    graph_builder.add_edge("writer", "evaluator")
+    graph_builder.add_conditional_edges("evaluator", should_continue, ["writer",END])
 
-graph = graph_builder.compile()
+    memory = MemorySaver()
 
-show_graph(graph)
-config = {"configurable": {"thread_id": "1"}}
+    graph = graph_builder.compile(checkpointer=memory)
 
-while True:
-    user_input = input("Query: ")
-    #user_input = 'llm-based agents'
-    if user_input.lower() in ["quit", "exit", "q"]:
-        print("Goodbye!")
-        break
-# The config is the **second positional argument** to stream() or invoke()!
-    events = graph.stream(
-        {"messages": [{"role":"user","content": user_input}]}, config, stream_mode="debug"
-    )
-    for event in events:
-        #print(event)
-        input('Press Enter to continue')
-        #event["messages"][-1].pretty_print() #stream_mode='value'
-        #stream_mode='debug'
-        '''if event['type'] == 'checkpoint':
-            if event['payload']['values']["messages"]:
-                event['payload']['values']["messages"][-1].pretty_print() 
-        elif event['type'] == 'task':
-            event['payload']['input']["messages"][-1].pretty_print() 
-        elif event['type'] == 'value':
-            event["messages"][-1].pretty_print()
-        elif event['type'] == 'task_result':
-            print('task')
-            event["payload"]['result'][0][-1][0].pretty_print()
-        else:
-            event["messages"][-1].pretty_print()'''
-        print(event['type'])
-
+    return graph
