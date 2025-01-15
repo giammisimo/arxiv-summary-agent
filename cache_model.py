c0cea4a079ee1af60142e8f91030237cbd2c122f
Gian Marco Simonazzi <gianmarco.simonazzi@studenti.unipr.it>
2025-01-15 17:50:02 +0100

    Added readme and some comments

--- cache_model.py
+++ cache_model.py
@@ -1,3 +1,5 @@
+## Used to offset the download of the model to docker image build time
+
 from sentence_transformers import SentenceTransformer
 
 embedding_model = SentenceTransformer("all-mpnet-base-v2")
